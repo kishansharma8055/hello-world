60ee5e04b67a3b7ec99fc8528c204292768820bd
kishandadhich <114287104+kishandadhich@users.noreply.github.com>
2022-10-20 21:39:07 +0530

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,8 @@
+// this is my first program in c++
+// this program is print a hello world 
+#include<iostream>            // header file
+using namespace std;          // must in c++      
+int main(){                   // main
+    cout<<"hello world!"<<endl;   // print a string 
+    return 0;                    // return 0 value to operating system
+}
